7195c6e8e9b19eab2b693d8a22a46bca2778992f
roger zhao <general_zyj@163.com>
2016-09-09 22:27:01 +0800

    seems like ttyUSB0 & ralink is ok now

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 1 */
 /*  PREEMPT RT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 PREEMPT RT Wed Sep 7 11:18:31 CST 2016"
+#define UTS_VERSION "#1 PREEMPT RT Fri Sep 9 20:58:17 CST 2016"
 #define LINUX_COMPILE_BY "zyj"
 #define LINUX_COMPILE_HOST "zyj-ubuntu64"
 #define LINUX_COMPILER "gcc version 4.9.3 20150413 (prerelease) (Linaro GCC 4.9-2015.05) "
