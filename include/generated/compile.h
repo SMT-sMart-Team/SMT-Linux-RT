34a8f6901171907e386eebd9de46da0e81cbd387
roger zhao <general_zyj@163.com>
2016-09-11 22:10:03 +0800

    fix imu CS to CS0 by typo

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 0 */
 /*  PREEMPT RT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 PREEMPT RT Sat Sep 10 23:12:25 CST 2016"
+#define UTS_VERSION "#0 PREEMPT RT Sun Sep 11 22:06:19 CST 2016"
 #define LINUX_COMPILE_BY "zyj"
 #define LINUX_COMPILE_HOST "zyj-ubuntu64"
-#define LINUX_COMPILER "gcc version 5.4.0 20160609 (Ubuntu/Linaro 5.4.0-6ubuntu1~16.04.1) "
+#define LINUX_COMPILER "gcc version 4.9.3 20150413 (prerelease) (Linaro GCC 4.9-2015.05) "
