714d7c914dbd1c3de3cb1d3dbfdb47ba39b13e42
roger zhao <general_zyj@163.com>
2017-04-08 21:18:35 +0800

    repair kernel_config link wrong: SY-G2R is OK one

--- cross_compile.sh
+++ cross_compile.sh
@@ -26,7 +26,7 @@ cp arch/arm/boot/dts/Makefile `pwd`/output/dtbs
 # cp arch/arm/boot/dts/am335x-bone-common.dtsi `pwd`/output/dtbs
 
 echo "tar zImage dtbs & modules"
-cd `pwd`/output; tar -czvf ../`date +%Y%m%d`_apm-kernel-4.1.13_apm-config-adis16400_USB-DMA_debug_i2c.tar.gz *
+cd `pwd`/output; tar -czvf ../`date +%Y%m%d`_kernel-delte-rt2800-msg-add-pwmout.tar.gz *
 
 echo "end execute $0"
 echo "==================================================================================="
--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 0 */
 /*  PREEMPT RT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 PREEMPT RT Mon Sep 12 07:29:12 CST 2016"
+#define UTS_VERSION "#0 PREEMPT RT Sat Apr 8 21:01:49 CST 2017"
 #define LINUX_COMPILE_BY "zyj"
 #define LINUX_COMPILE_HOST "zyj-ubuntu64"
 #define LINUX_COMPILER "gcc version 4.9.3 20150413 (prerelease) (Linaro GCC 4.9-2015.05) "
